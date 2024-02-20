ce16c9c94a7ffba64b114d3a8eecde26bd71646b
AIKauSs <kaismansour22@gmail.com>
2024-02-20 11:50:07 -0500

    login/register routes added + mongo connection

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-app.set('view engine', ejs)
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+// Setup connection to database (MongoDB)
+const mongoose = require('mongoose'); 
+const DB_URI = "mongodb://localhost/projet7"
+mongoose.connect(DB_URI)
+.then(() => console.log("Successfully connected to MongoDB"))
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process if unable to connect to MongoDB
+});
+
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/view');
 
-//page d'accueil
-app.get('/',( req,res) =>{
+// Page d'accueil
+app.get('/', async (req, res) => {
+    console.log("Step1")
+    res.render('index')
+});
 
-//mettre fichier html ici 
-res.render(__dirname + '/view/index.ejs');
-})
+// Router de login-register
+const userRouter = require('./routes/user');
+app.use('/user', userRouter);
 
-//router de login-register
-const welcomeRouter = require('./routes/user')
-app.use('/user', welcomeRouter)
-
-
-
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
