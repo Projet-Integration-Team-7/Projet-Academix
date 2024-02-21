177c1e582a1a3817cfd8e2e194c21e6a861d3a95
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-02-21 11:34:09 -0500

    Svelte_Integ_V1

---
+++ ui/svelteapp/src/components/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+
+<div>
+  <h3>Page Accueil</h3>
+  <Link to="blog">Blog</Link><br />
+  <Link to="about">About</Link><br/>
+  <Link to="settingspage">Settings</Link>
+</div>
---
+++ ui/svelteapp/src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  import Home from "../components/Home.svelte";
+  import About from "../components/About.svelte";
+  import Blog from "../components/Blog.svelte";
+  import SettingsPage from "../components/Settingspage.svelte";
+
+  export let url = "";
+</script>
+
+<Router {url}>
+  <div>
+    <Route path="blog" component={Blog} />
+    <Route path="about" component={About} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
