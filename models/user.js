53ba0e71e761db17c1f81053723b871033f0d568
AIKauSs <kaismansour22@gmail.com>
2024-02-28 08:23:49 -0500

    Register page updates (backend)

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    prenom: { type: String, required: true },
+    nom: {type: String, required: true},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    genre: {type: String, required: true},
+    dn: {type: Date, required: true},
+    tel:{type :String, required:true},
     createdAt: { type: Date, default: Date.now }
 });
 
