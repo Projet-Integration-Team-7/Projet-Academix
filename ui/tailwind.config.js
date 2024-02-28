e555954f7cc0a38c8c5229face9272dc11a18334
Spyrolol <sgeronimo007@gmail.com>
2024-02-28 11:39:18 -0500

    Settings_Page
    J'ai fait page settings,les options de base sont,faut link avec serveur pour ensuite rajouter plus option.

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
+}
+module.exports = {
+  // ...
+  variants: {
+    extend: {
+      textOpacity: ['dark']
+    }
+  }
 }
 
