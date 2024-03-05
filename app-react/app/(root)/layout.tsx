c79b53d21fdcf22ee99e850b8aad604613b24e68
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-03-05 13:59:50 -0500

    Session_fixed
    jai configurer rooting comme ca quand tu login sur le site tarrive sur ta session et jai fait de sorte que si test login le site ca sera tjr ta session meme si tu quitte sans logout

--- app-react/app/(root)/layout.tsx
+++ app-react/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +15,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
+      </html>
+    </ClerkProvider>
   );
+ 
 }
