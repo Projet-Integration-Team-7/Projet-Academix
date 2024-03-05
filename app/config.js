5974afd84ac4f9c5ee3e82edd52a4c40049f05ee
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-03-05 01:34:25 -0500

    Version_ULTIME
    cette version marche,on peut acceder data,supprimer et ajouter,je preconise cette version

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 module.exports={
     port: process.env.PORT || 3000,
-    mongoURI:"mongodb+srv://bzyounes05:sIsEsSYkdISHCs1a@academix-data-base.zuzp88h.mongodb.net/?retryWrites=true&w=majority&appName=Academix-Data-Base",
+    mongoURI:"mongodb+srv://bzyounes05:BgTqFkueeaiqg14C@academix-data-base.zuzp88h.mongodb.net/?retryWrites=true&w=majority&appName=Academix-Data-Base",
 }
 
 
