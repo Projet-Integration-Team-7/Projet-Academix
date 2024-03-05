845c766e3dd8541bfe15e1ba2a86fd2928412794
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-03-04 21:40:00 -0500

    Probleme Code Gaetan resolu
    version de mongo poser problem,jai installer version3  comme le gard video

--- app/models/Transaction.js
+++ app/models/Transaction.js
@@ -1,20 +1,21 @@
-const {Schema,model}=require('mongoose')
-const now=new Date().getTime()
-const TransactionSchema=new Schema({
-    value:{
-        type:Number,
-        required:true,
+const { Schema, model } = require('mongoose')
 
+const now = new Date().getTime()
+const TransactionSchema = new Schema({
+    value: {
+        type: Number,
+        required: true,
     },
-    date:{
-        type:Number,
-        default:now
-
+    date: {
+        type: Number,
+        default: now,
+    },
+    user_id: {
+        type: String,
+        required: true,
     },
 })
 
+const Transaction = model('transaction', TransactionSchema)
 
-
-const Transaction=model('transaction',TransactionSchema)
-
-module.exports=Transaction
+module.exports = Transaction
--- app/models/User.js
+++ app/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const {Schema} = mongoose
+const { Schema } = mongoose
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const User = new Schema({})
 
-User.plugin(passportLocalMongoose())
+User.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model('user', User)
