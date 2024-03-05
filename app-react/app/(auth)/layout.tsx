329c0870600655c2d122a13465acb7fad3ddb866
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-03-05 13:40:32 -0500

    Sign-In et Sign-Up marchent
    jai utiliser clerk pour pouvoir creer mon register et login.du coup la on peut creer des comptes et se connecter,ca envoie par email le code confirmation.Jai essayer jai creer un compte et dans clerk je peux voir tout les user er leur infroamtion,jpeux meme ban des user

---
+++ app-react/app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import{ClerkProvider} from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import '../globals.css'
+
+export const metadata={
+    tittle:'Academix',
+    description:'A Next.js 13 Meta Academix app'
+
+}
+
+
+const inter=Inter({subsets :["latin"]})
+
+
+export default function RootLayout({
+    children
+}:{
+    children:React.ReactNode
+}){
+    return (
+    <ClerkProvider>
+        <html lang="en">
+            <body className={`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+
+        </html>
+    </ClerkProvider>
+    )
+}
