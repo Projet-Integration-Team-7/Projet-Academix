3cf78d5c746572c64b79ff9eb7a41d22b0f7a65a
Gaetan Lohier <lohiergaetan.pro@gmail.com>
2024-03-06 21:22:05 -0500

    fetchPosts, ThreadCard structure et Commentaires

--- app-react/app/(root)/layout.tsx
+++ app-react/app/(root)/layout.tsx
@@ -1,13 +1,13 @@
+import React from "react";
 import type { Metadata } from "next";
 import "@uploadthing/react/styles.css";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
-import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Bottombar from "@/components/shared/Bottombar";
 import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,34 +19,26 @@ export const metadata={
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  
+}) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang='en'>
         <body className={inter.className}>
           <Topbar />
 
-          <main className="flex flex-row">
+          <main className='flex flex-row'>
             <LeftSidebar />
-
-            <section className="main-container">
-              <div className="w-full max-w-4xl">
-                {children}
-              </div>
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
-
             <RightSidebar />
           </main>
 
-          
-          {children}
           <Bottombar />
-          </body>
+        </body>
       </html>
     </ClerkProvider>
   );
- 
 }
