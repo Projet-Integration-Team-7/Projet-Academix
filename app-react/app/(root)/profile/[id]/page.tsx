2ceda0d32633fdc4e78d3f7599030d0b7cb5342b
Adam4210 <127917702+Adam4210@users.noreply.github.com>
2024-03-07 01:01:51 -0500

    creation du squelette de Profile header et de la page profil

---
+++ app-react/app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from "next/navigation";
+import {fetchUser} from '@/lib/actions/user.actions'
+import { string } from 'zod';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+
+async function Page({ params } :{ params : { id : string}}){
+    const user=await currentUser();
+
+
+    if(!user) return null;
+
+
+    const userInfo=await fetchUser(params.id);
+
+
+    if(!userInfo?.onboarded)redirect("/onboarding");
+    return(
+    <section>
+        <ProfileHeader>
+             accountId={userInfo.id}
+             authUserId={user.id}
+             name={userInfo.name}
+             username={userInfo.username}
+             imageUrl={userInfo.image}
+             bio={userInfo.bio}
+        </ProfileHeader>
+    </section>
+    )
+}
+export default Page;
