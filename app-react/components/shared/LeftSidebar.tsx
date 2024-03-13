537da5b1983a8c59b7ec5e150db6eafff6190041
Adam4210 <127917702+Adam4210@users.noreply.github.com>
2024-03-13 13:05:09 -0400

    bug profile réglé

--- app-react/components/shared/LeftSidebar.tsx
+++ app-react/components/shared/LeftSidebar.tsx
@@ -19,8 +19,7 @@ function LeftSidebar() {
                 const isActive = (pathname.includes(link.route) && link.route.
                 length > 1) || pathname === link.route;
 
-                if(link.route ==='/profile') link.route =`${link.route}/$
-                {userId}`
+                if(link.route ==='/profile') link.route =`${link.route}/${userId}`
             
                 return (
                     <div>
