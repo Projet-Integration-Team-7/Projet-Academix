d9a109afb4262ce73b230b13477514b50c0cbce4
Gaetan Lohier <lohiergaetan.pro@gmail.com>
2024-03-13 01:44:44 -0400

    debut

---
+++ app-react/components/shared/LikeBtn.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+
+
+function LikeBtn() {
+    let imgUrl : string ="";
+    const grayHeartUrl = "/assets/heart-gray.svg";
+    const filledHeartUrl = "/assets/heart-filled.svg"
+    let liked = false;
+
+    const getStatut = (a : boolean) => {
+        if (a) {
+            imgUrl = filledHeartUrl;
+        }
+        else {
+            imgUrl = grayHeartUrl;
+        }
+    }
+
+    return (
+        {getStatut(liked)};
+        <Image src={imgUrl} alt="heart" width={24} height={24} className="cursor-pointer object-contain" />
+
+    )
+
+
+
+
+
+}
+
+export default LikeBtn;
