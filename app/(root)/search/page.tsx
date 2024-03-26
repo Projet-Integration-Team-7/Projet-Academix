5b2e76361196ab5937ff8bfeefc8b84c5154d2ee
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-03-26 07:24:06 -0400

    Improvement 1
    amelioration code

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -18,41 +18,40 @@ async function Page({
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
+  const { userId, q: searchString, page = '1' } = searchParams;
+  const pageNumber = Number(page);
+  const pageSize = 25;
+
   const result = await fetchUsers({
     userId: user.id,
-    searchString: searchParams.q,
-    pageNumber: searchParams?.page ? +searchParams.page : 1,
-    pageSize: 25,
+    searchString,
+    pageNumber,
+    pageSize,
   });
 
   return (
     <section>
       <h1 className='head-text mb-10'>Search</h1>
-
       <Searchbar routeType='search' />
-
       <div className='mt-14 flex flex-col gap-9'>
         {result.users.length === 0 ? (
           <p className='no-result'>No Result</p>
         ) : (
-          <>
-            {result.users.map((person) => (
-              <UserCard
-                key={person.id}
-                id={person.id}
-                name={person.name}
-                username={person.username}
-                imgUrl={person.image}
-                personType='User'
-              />
-            ))}
-          </>
+          result.users.map((person) => (
+            <UserCard
+              key={person.id}
+              id={person.id}
+              name={person.name}
+              username={person.username}
+              imgUrl={person.image}
+              personType='User'
+            />
+          ))
         )}
       </div>
-
       <Pagination
         path='search'
-        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        pageNumber={pageNumber}
         isNext={result.isNext}
       />
     </section>
