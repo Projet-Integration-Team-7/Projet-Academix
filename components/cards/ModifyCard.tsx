ede81b36d5251181bfdbe15153ee983a2f3b3647
Adam4210 <127917702+Adam4210@users.noreply.github.com>
2024-04-01 18:15:47 -0400

    ajout de l'icone

--- components/cards/ModifyCard.tsx
+++ components/cards/ModifyCard.tsx
@@ -9,9 +9,18 @@ const ModifyCard=({} :Props) =>{
     const router=useRouter();
 
     return(
-  <Button className="user-card_btn" onClick={() => router.push(`/modifyProfile`) }>
-           <img src="image.ico" alt="aallo" />     <h1>   Modify Profile   </h1> 
+      <div className="flex items-center">
+      <Image
+      className="rounded-full"
+      width={45}
+      height={50}
+       src="/assets/image.png"
+        alt="aallo" /> 
+<Button className="user-card_btn w-48 h-12 mx-2" onClick={() => router.push(`/modifyProfile`)}>
+    
+              <h1>      Modify Profile   </h1> 
             </Button>
+            </div>
     )
 };
 export default ModifyCard
