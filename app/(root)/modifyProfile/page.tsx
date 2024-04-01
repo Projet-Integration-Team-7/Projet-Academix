560259f3275fab38e412aebf059330d791cd74c0
Adam4210 <127917702+Adam4210@users.noreply.github.com>
2024-04-01 01:15:58 -0400

    UI modify Profile termine

--- app/(root)/modifyProfile/page.tsx
+++ app/(root)/modifyProfile/page.tsx
@@ -1,7 +1,32 @@
-const Page =async () =>{
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from "next/navigation";
+import {fetchUser} from '@/lib/actions/user.actions'
+import PostThread from '@/components/forms/PostThread';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import EditCard from '@/components/cards/EditCard';
+
+async function Page(){
+    const user=await currentUser();
+
+
+    if(!user) return null;
+
+
+    const userInfo=await fetchUser(user.id);
+
+
+    if(!userInfo?.onboarded)redirect("/onboarding");
     return(
         <section>
-        <h1 className="head-text mb-10">Modify Profile  </h1>¸
+        <h1 className="head-text mb-10">Modify Profile  </h1>
+        <EditCard
+              key={user.id + "-profile"}
+              id={user.id}
+              name={""}
+              username={user.username}
+              imgUrl={user.imageUrl}
+              personType='User'
+            />
         </section>
     )
 }
