2923add1cfdfcbbb234c5f2295836abda8b8b8fb
GaetanAL <150467427+GaetanAL@users.noreply.github.com>
2024-04-03 09:30:42 -0400

    notification model

---
+++ lib/models/notification.model.ts
@@ -0,0 +1,27 @@
+import mongoose   from "mongoose";
+
+const notificationSchema=new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+      },
+    message: {
+         type: String,
+        required: true,
+     },
+     createdAt: {
+        type: Date,
+        default: Date.now,
+     },
+     notifType: {
+        type: String,
+        required: true,
+     },
+    read: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const Notification = mongoose.models.Notification ||mongoose.model('Notification',notificationSchema);
+export default Notification;
