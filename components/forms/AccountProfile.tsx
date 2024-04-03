a2cefeafd5f3e64e05ffebb1bd6fefd31c6813c9
Adam4210 <127917702+Adam4210@users.noreply.github.com>
2024-04-03 12:12:08 -0400

    url de la photo choisi enregistre

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -55,29 +55,29 @@ const pathname=usePathname();
             bio:user?.bio||""
          }
     })
-    //charger l image donner,et de sorte quelle apparati quand tu upload
-    const handleImage = (
-      e: ChangeEvent<HTMLInputElement>,
-      fieldChange: (value: string) => void
-    ) => {
-      e.preventDefault();
-  
-      const fileReader = new FileReader();
-  
-      if (e.target.files && e.target.files.length > 0) {
-        const file = e.target.files[0];
-        setFiles(Array.from(e.target.files));
-  
-        if (!file.type.includes("image")) return;
-  
-        fileReader.onload = async (event) => {
-          const imageDataUrl = event.target?.result?.toString() || "";
-          fieldChange(imageDataUrl);
-        };
-  
-        fileReader.readAsDataURL(file);
-      }
-    };
+    //charger l'image donner,et de sorte quelle apparati quand tu upload
+      const handleImage = (
+        e: ChangeEvent<HTMLInputElement>,
+        fieldChange: (value: string) => void
+      ) => {
+        e.preventDefault();
+    
+        const fileReader = new FileReader();
+    
+        if (e.target.files && e.target.files.length > 0) {
+          const file = e.target.files[0];
+          setFiles(Array.from(e.target.files));
+    
+          if (!file.type.includes("image")) return;
+    
+          fileReader.onload = async (event) => {
+            const imageDataUrl = event.target?.result?.toString() || "";
+            fieldChange(imageDataUrl);
+          };
+    
+          fileReader.readAsDataURL(file);
+        }
+      };
     
     // 2. Define a submit handler.
   const onSubmit=async (values:z.infer<typeof UserValidation>)=> {
