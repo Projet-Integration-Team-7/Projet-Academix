0ff88fdff95997150425b70fb767b103937d9734
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-04-19 15:28:43 -0400

    Ui main V1
    Top bar et left bar sont transformer ,style choisi style blue dark-lime-jaune-bleu clair,avec ajout dimpression ressorti pour les bouttons

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -72,7 +72,7 @@ const ThreadCard = ({
         console.log(threadType)
         return (
         
-        <article className={`flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7' : ' bg-dark-2 p-7'}`}>
+<article className={`card-hover-effect flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
             <div className="static flex items-start justify-between">
                 <div className="flex w-full flex-1 flex-row gap-4">
                     <div className="flex flex-col items-center">
