416c2223577638c74050c0518084da4a0e3387ae
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-04-23 17:24:15 -0400

    auto-refresh_v3

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,40 +1,31 @@
+import { useEffect } from 'react';
 import ThreadCard from "@/components/cards/ThreadCard";
 import Comment from "@/components/forms/Comment";
 import { fetchThreadById } from "@/lib/actions/thread.action";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import React, { useState, useEffect } from 'react';
 
 const Page = async ({ params}: {params: { id: string}}) => {
-    const [thread, setThread] = useState(null);
-    const [user, setUser] = useState(null);
-    const [userInfo, setUserInfo] = useState(null);
-    useEffect(() => {
-        const init = async () => {
-            if (!params.id) return;
-    
-            const userResponse = await currentUser();
-            if (!userResponse) return;
-            setUser(userResponse);
-    
-            const userInfoResponse = await fetchUser(userResponse.id);
-            if (!userInfoResponse?.onboarded) {
-                redirect('/onboarding');
-                return;
-            }
-            setUserInfo(userInfoResponse);
-    
-            const threadResponse = await fetchThreadById(params.id);
-            setThread(threadResponse);
-        };
-        init();
-    
-        const interval = setInterval(init, 5000);  // Refresh every 5 seconds
-        return () => clearInterval(interval);  // Clean up interval on component unmount
-    }, []);  // Empty dependency array
+    if(!params.id) return null;
 
-    if (!thread || !user || !userInfo) return null;
+    const user = await currentUser();
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect('/onboarding')
+
+    const thread = await fetchThreadById(params.id)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetchThreadById(params.id).then(updatedThread => {
+                thread = updatedThread;
+            });
+        }, 5000); // 30 seconds
+
+        return () => clearInterval(interval);
+    }, [params.id]);
+
    return (
         <section className="relative">
             <div>
