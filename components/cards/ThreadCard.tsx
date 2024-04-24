73713c694fb8a8c83a667cc8dd805035a74f3a69
GaetanAL <150467427+GaetanAL@users.noreply.github.com>
2024-04-24 10:06:45 -0400

    Revert "remove threadMenu dupes"
    This reverts commit 50000286dfdc13e765e62f245fbb241bb5c7095b.

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -44,51 +44,45 @@ interface Props {
   isComment?: boolean;
   threadType: string;
 }
-
+ 
 const ThreadCard = ({
-  id,
-  currentUserId,
-  currentUser,
-  parentId,
-  content,
-  author,
-  community,
-  imgUrl,
-  createdAt,
-  comments,
+    id,
+    currentUserId,
+    currentUser,
+    parentId,
+    content,
+    author,
+    community,
+    imgUrl,
+    createdAt,
+    comments,
+    
+    likes,
+    isComment,
+    threadType,
+    // Access the list of members of the community
+    
+    
+}: Props) => {
+    
 
-  likes,
-  isComment,
-  threadType,
-}: // Access the list of members of the community
-
-Props) => {
-  // Your component logic here
-  console.log(threadType);
-  return (
-    <article
-      className={`card-hover-effect flex w-full flex-col rounded-xl ${
-        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
-      }`}
-    >
-      <div className="static flex items-start justify-between">
-        <div className="flex w-full flex-1 flex-row gap-4">
-          <div className="flex flex-col items-center">
-            {author && (
-              <Link
-                href={`/profile/${author.id}`}
-                className="relative h-11 w-11"
-              >
-                <Image
-                  src={author.image}
-                  alt="Profile image"
-                  fill
-                  className="cursor-pointer rounded-full"
-                />
-              </Link>
-            )}
-            <div className="thread-card_bar" />
-          </div>
+    
+        // Your component logic here
+        console.log(threadType)
+        return (
+        
+<article className={`card-hover-effect flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
+            <div className="static flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-row gap-4">
+                    <div className="flex flex-col items-center">
+                        
+                    {author && (
+    <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+        <Image src={author.image} alt="Profile image" fill className="cursor-pointer rounded-full" />
+    </Link>
+)}
+                        <div className="thread-card_bar"/>
+                    </div>
 
           <div className="flex w-full flex-col">
             <div className=" inline-flex justify-between">
@@ -99,7 +93,19 @@ Props) => {
                       {author.name}
                     </h4>
                   </Link>
+                  <ThreadMenu
+                    threadId={id}
+                    currentUserId={currentUserId}
+                    authorId={author.id}
+                  />
                 </div>
+              )}
+              {author && (
+                <ThreadMenu
+                  threadId={id}
+                  currentUserId={currentUserId}
+                  authorId={author.id}
+                />
               )}
             </div>
             {imgUrl && imgUrl !== "" && imgUrl !== imgPlacebot && (
@@ -118,71 +124,65 @@ Props) => {
 
             <p className="mt-2 text-small-mono text-black">{content}</p>
 
-            <div className=" flex mb-10 mt-5 flex-col gap-3 align-middle">
-              <div className=" flex gap-3.5 align-middle">
-                <LikeBtn
-                  threadId={JSON.parse(JSON.stringify(id))}
-                  currentUserId={currentUserId}
-                  mapLikes={likes}
-                  likesCount={likes.size}
-                />
-                <Link href={`/thread/${id}`}>
-                  <Image
-                    src="/assets/reply.svg"
-                    alt="reply"
-                    width={24}
-                    height={24}
-                    className="cursor-pointer object-contain"
-                  />
-                </Link>
-                {/* <Image src="/assets/repost.svg" alt="repost" width={24} height={24} className="cursor-pointer object-contain" /> FONCTION REPOST FACULTATIF +- */}
-                <ShareBtn threadId={JSON.parse(JSON.stringify(id))} />
-              </div>
+                        <div className=" flex mb-10 mt-5 flex-col gap-3 align-middle">
+                            <div className=" flex gap-3.5 align-middle">
+                                <LikeBtn threadId={JSON.parse(JSON.stringify(id))} currentUserId={currentUserId} mapLikes={likes} likesCount={likes.size} />
+                                <Link href={`/thread/${id}`}>
+                                    <Image src="/assets/reply.svg" alt="reply" width={24} height={24} className="cursor-pointer object-contain" />
+                                </Link>
+                                {/* <Image src="/assets/repost.svg" alt="repost" width={24} height={24} className="cursor-pointer object-contain" /> FONCTION REPOST FACULTATIF +- */}
+                                <ShareBtn threadId={JSON.parse(JSON.stringify(id))} />
+                            </div>
 
-              {isComment && comments.length > 0 && (
-                <Link href={`/thread/${id}`}>
-                  <p className="mt-1 text-subtle-medium text-gray-1">
-                    {comments.length} replies
-                  </p>
-                </Link>
-              )}
-            </div>
-          </div>
-        </div>
-        <ThreadMenu
-          threadId={JSON.parse(JSON.stringify(id))}
-          currentUserId={currentUserId}
-          authorId={author ? author.id ?? "" : ""}
-        />{" "}
-      </div>
+                            {isComment && comments.length > 0 && (
+                                <Link href={`/thread/${id}`}>
+                                    <p className="mt-1 text-subtle-medium text-gray-1">
+                                        {comments.length} replies
+                                    </p>
+                                </Link>
+                            )}
+                        </div>
+                        
+                    </div>
+                </div>
+                
+               
 
-      <p className="text-subtle-medium text-gray-1">
-        {threadType === "exercise" && "Exercise"}
-        {threadType === "course_note" && "Note de cours"}
-        {threadType === "evaluation" && "Evaluations"}
-      </p>
 
-      {!isComment && community && (
-        <Link
-          href={`/communities/${community.id}`}
-          className="mt-5 flex items-center"
-        >
-          <p className="text-subtle-medium text-gray-1">
-            {formatDateString(createdAt)}{" "}
-            {community && ` - ${community.name} Communite`}
-          </p>
+                <ThreadMenu threadId={JSON.parse(JSON.stringify(id))} currentUserId={currentUserId} authorId={author ? author.id ?? '' : ''}/>                 </div>
+            
+            <p className='text-subtle-medium text-gray-1'>
+                {threadType === "exercise" && "Exercise"}
+                {threadType === "course_note" && "Note de cours"}
+                {threadType === "evaluation" && "Evaluations"}
+                    </p>
 
-          <Image
-            src={community.image}
-            alt={community.name}
-            width={14}
-            height={14}
-            className="ml-1 rounded-full object-cover"
-          />
-        </Link>
-      )}
-    </article>
-  );
-};
+            {!isComment&& community&&(
+                    <Link
+                    href={`/communities/${community.id}`}
+                    className='mt-5 flex items-center'
+                  >
+                    
+                    <p className='text-subtle-medium text-gray-1'>
+                      {formatDateString(createdAt)}
+                      {" "}{community && ` - ${community.name} Communite`}
+                    </p>
+                    
+                    <Image
+                      src={community.image}
+                      alt={community.name}
+                      width={14}
+                      height={14}
+                      className='ml-1 rounded-full object-cover'
+                    />
+                  </Link>
+                  
+                )}
+        </article>
+            
+          )
+            
+
+}
 
 export default ThreadCard;
