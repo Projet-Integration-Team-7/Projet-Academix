ec3bfb9a0cf5b132fa255ec1d8de0e9169a170fb
Adam4210 <127917702+Adam4210@users.noreply.github.com>
2024-04-24 09:06:27 -0400

    bug regle avec author.id

--- components/forms/Pomodoro.tsx
+++ components/forms/Pomodoro.tsx
@@ -86,7 +86,7 @@ const reset =()=>{
       return (
 
         <section>
-            <h1  className="head-text mb-10"> Timer   </h1>
+            <h1  className="head-text mb-10">   </h1>
             <section className="mt-10 flex flex-col gap-5">
               <div className='max-w-2xl min-h-screen mx-auto'>
               <Navigation/>
