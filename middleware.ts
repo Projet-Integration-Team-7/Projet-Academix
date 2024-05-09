2564efe4b317df14c79b6d9b1e46b1831bb462be
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-05-09 14:18:29 -0400

    fix2

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import "@uploadthing/react/styles.css";
 export default authMiddleware({
   // Routes that can be accessed while signed out
   publicRoutes: ['/', '/api/webhook/clerk', '/api/uploadthing'],
-  // ignoredRoutes: ['/api/clerk/webhook']
+   ignoredRoutes: ['/api/clerk/webhook']
 });
  
 export const config = {
